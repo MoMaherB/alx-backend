c262300070bce4376cad67bc0c7ec859e71c7b7f
MoMaherB <maher1525@gmail.com>
2024-10-31 00:22:19 +0300

    cashing

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""0. Basic dictionary"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """  class BasicCache that inherits from BaseCaching
+    and is a caching system:
+    """
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        self.cache_data[key] = item
