f8fe8cf4135dd1c776b5a4fead19f9cddaa0d7dd
MoMaherB <maher1525@gmail.com>
2024-10-31 00:26:45 +0300

    cashing

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,4 +17,5 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
